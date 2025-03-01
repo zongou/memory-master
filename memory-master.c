e06b42cb03b9863bba5210e3d23d0a44b3b42fe4
a <a@example.com>
2025-03-01 21:44:18 +0800

    refactor(memory-master): replace wide characters with narrow characters
    - Remove inclusion of <wchar.h>
    - Change wchar_t to char for strings and array elements
    - Update string manipulation functions from wcs* to str*
    - Replace wprintf with printf for output
    - Free allocated memory for each card in the deck

--- memory-master.c
+++ memory-master.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <sys/random.h>
 #include <unistd.h>
-#include <wchar.h>
 
 int period = 0;
 
@@ -41,12 +40,12 @@ int printRandomDigitMatrix(int column, int row, int width) {
 }
 
 // Function to shuffle the array of cards
-void shuffle(wchar_t **array, size_t n) {
+void shuffle(char **array, size_t n) {
   if (n > 1) {
     size_t i;
     for (i = 0; i < n - 1; i++) {
       size_t j = i + getRandom() % (n - i);
-      wchar_t *t = array[j];
+      char *t = array[j];
       array[j] = array[i];
       array[i] = t;
     }
@@ -54,19 +53,18 @@ void shuffle(wchar_t **array, size_t n) {
 }
 
 int card_shuffle() {
-  // Set the locale to support wide characters
-  setlocale(LC_ALL, "");
-
-  wchar_t *suits[] = {L"♥️", L"♦️", L"♠️", L"♣️"};
-  wchar_t *ranks[] = {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", L"9", L"0", L"J", L"Q", L"K"};
-  wchar_t *deck[52];
+  char *suits[] = {"♥️", "♦️", "♠️", "♣️"};
+  char *ranks[] = {"1", "2", "3", "4", "5", "6", "7",
+                   "8", "9", "0", "J", "Q", "K"};
+  char *deck[52];
   int index = 0;
 
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 13; j++) {
-      deck[index++] = (wchar_t *)malloc((wcslen(suits[i]) + wcslen(ranks[j]) + 1) * sizeof(wchar_t));
-      wcscpy(deck[index - 1], ranks[j]);
-      wcscat(deck[index - 1], suits[i]);
+      deck[index++] = (char *)malloc((strlen(suits[i]) + strlen(ranks[j]) + 1) *
+                                     sizeof(char));
+      strcpy(deck[index - 1], ranks[j]);
+      strcat(deck[index - 1], suits[i]);
     }
   }
 
@@ -76,10 +74,10 @@ int card_shuffle() {
 
   // Print the shuffled cards
   for (int i = 0; i < totalCards; i++) {
-    wprintf(L"%ls \t", deck[i]);
+    printf("%s \t", deck[i]);
     periodically_output(period);
     if ((i + 1) % 4 == 0) { // Print a newline every 4 cards
-      wprintf(L"\n");
+      printf("\n");
     }
     free(deck[i]); // Free allocated memory
   }
@@ -87,7 +85,6 @@ int card_shuffle() {
 }
 
 int main(int argc, char *argv[]) {
-
   char *help_info = "\
 Memory Master\n\
 \n\
